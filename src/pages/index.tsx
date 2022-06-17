e088437ee7f3a5182c17ba7f0958c613ed4fecb1
Corey DeLoach <cdeloach16@gmail.com>
2022-06-16 20:02:25 -0500

    added tRPC, setup api endpoint, structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,21 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { trpc } from "@/utils/trpc";
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "world" }]);
+  if (isLoading) return <p>Loading...</p>;
+
+  if (data) return <p>{data.greeting}</p>;
   return (
     <div className={styles.container}>
       <Head>
         <title>tRPC | Demo</title>
-        <meta name="description" content="Demo using tRPC, Prisma, and Planetscale" />
-        <meta name="color-scheme" content="dark" />
+        <meta
+          name="description"
+          content="Demo using tRPC, Prisma, and Planetscale"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
